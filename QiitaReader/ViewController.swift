2c7d4d9db037f585f3dc60fb6c58194b8bfb6006
Yo Higashida <penguin4731@gmail.com>
2021-02-17 14:18:23 +0900

    Qiitaから記事一覧を読み込み

--- QiitaReader/ViewController.swift
+++ QiitaReader/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // QiitaAPIを呼び出してデータを取得する
+        // APIKeyの取得
+        let apiKey: String = KeyManager().getValue(key: "apiKey") as! String
+        // URLの設定
+        let url: URL = URL(string: "https://qiita.com/api/v2/items")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        // headerの設定
+        request.addValue(apiKey, forHTTPHeaderField: "Bearer")
+        // タスクの設定
+        let task: URLSessionTask = URLSession.shared.dataTask(with: request) {data, response, error in
+            if let data = data, let response = response {
+                print(response)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
+                    print(json)
+                } catch {
+                    print("Serialize Error")
+                }
+            } else {
+                print(error ?? "Error")
+            }
+        }
+        // タスクの実行
+        task.resume()
     }
-
-
+    
+    
 }
 
